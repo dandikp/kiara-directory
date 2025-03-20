f59da286685f334b11ef9458da52d5980ed78470
Dandi Pratama <mas.gondronglabanan@gmail.com>
2025-03-20 16:20:54 +0700

    feat: add seed

---
+++ prisma/seed.config.ts
@@ -0,0 +1,93 @@
+export const ROLES = [
+  {
+    id: 1,
+    name: "System Administrator",
+    level: 1,
+  },
+  {
+    id: 2,
+    name: "Direktur Utama",
+    level: 2,
+  },
+  {
+    id: 3,
+    name: "Direktur",
+    level: 3,
+  },
+  {
+    id: 4,
+    name: "Manager",
+    level: 4,
+  },
+  {
+    id: 5,
+    name: "Asisten Manager",
+    level: 5,
+  },
+  {
+    id: 6,
+    name: "Supervisor",
+    level: 6,
+  },
+  {
+    id: 7,
+    name: "Staff",
+    level: 7,
+  },
+];
+
+export const USERS = [
+  {
+    id: 1,
+    roleId: 1,
+    email: "admin@kinarya.com",
+    phone: "085123456789",
+    password: "123456",
+    name: "Admin",
+  },
+  {
+    id: 2,
+    roleId: 2,
+    email: "ceo@kinarya.com",
+    phone: "085987654321",
+    password: "123456",
+    name: "CEO",
+  },
+  {
+    id: 3,
+    roleId: 4,
+    email: "manager@kinarya.com",
+    phone: "085987321654",
+    password: "123456",
+    name: "Manager 1",
+  },
+  {
+    id: 4,
+    roleId: 6,
+    email: "koordinator@kinarya.com",
+    phone: "085654987321",
+    password: "123456",
+    name: "Koordinator 1",
+  },
+  {
+    id: 5,
+    roleId: 7,
+    email: "staff@kinarya.com",
+    phone: "085654321987",
+    password: "123456",
+    name: "Staff 1",
+  },
+];
+
+export const DIVISIONS = [
+  {
+    name: "Keuangan & Akuntansi",
+    code: "FINANCE",
+  },
+  {
+    name: "Divisi Contoh 2",
+    code: "EX_2",
+  },
+];
+
+export const COMPANIES = [];
---
+++ prisma/seed.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default prisma;
+
+// const divisions = async () => {
+//   const
+// };
+
+// const main = async () => {
+//   // const
+// };
