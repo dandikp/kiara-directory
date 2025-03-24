e6d309c0c7dda31bf77da633745a537afaffe616
Dandi Pratama <mas.gondronglabanan@gmail.com>
2025-03-24 16:33:09 +0700

    feat: add error lib

---
+++ lib/errors/messages.ts
@@ -0,0 +1,9 @@
+export const ERROR_MESSAGES = {
+  NOT_FOUND: "Tidak ditemukan.",
+  UNAUTHORIZED: "Anda tidak memiliki izin untuk mengakses halaman ini.",
+  INTERNAL_SERVER_ERROR:
+    "Terjadi kesalahan pada sistem. Silakan coba lagi nanti.",
+  BAD_REQUEST:
+    "Request tidak valid. Mohon periksa kembali data yang Anda kirimkan.",
+  FORBIDDEN: "Anda tidak memiliki izin untuk melakukan aksi ini.",
+};
