832b808e732d19ae56b2701564e73b42ff44309f
Dandi Pratama <mas.gondronglabanan@gmail.com>
2025-03-25 16:17:12 +0700

    feat: wip auth layout

--- app/(guest)/auth/layout.tsx
+++ app/(guest)/auth/layout.tsx
@@ -7,7 +7,7 @@ export default function AuthLayout({
 }>) {
   return (
     <div className="w-full min-h-dvh bg-image relative">
-      <div className="w-full mx-auto max-w-[90%] h-full backdrop-blur-sm bg-white/15 shadow-medium">
+      <div className="w-full mx-auto max-w-[90%] h-full backdrop-blur-sm bg-white/15 shadow-medium lg:mt-auto lg:max-w-48 lg:h-56">
         {children}
       </div>
     </div>
