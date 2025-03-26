47436282847f1e6e1fd8bbf64b3098e5d190fe56
Dandi Pratama <mas.gondronglabanan@gmail.com>
2025-03-26 16:22:59 +0700

    feat: add signin page

--- app/(guest)/auth/layout.tsx
+++ app/(guest)/auth/layout.tsx
@@ -1,4 +1,7 @@
+import { Card, CardContent } from "@/components/ui/card";
 import "./auth.css";
+import AuthHeader from "@/features/auth/components/auth-header";
+import Image from "next/image";
 
 export default function AuthLayout({
   children,
@@ -6,10 +9,22 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="w-full min-h-dvh bg-image relative">
-      <div className="w-full mx-auto max-w-[90%] h-full backdrop-blur-sm bg-white/15 shadow-medium lg:mt-auto lg:max-w-48 lg:h-56">
-        {children}
-      </div>
+    <div className="w-full min-h-dvh bg-image relative flex flex-col">
+      <AuthHeader className="hidden md:flex" />
+      <Card className="w-full mx-auto rounded-none md:rounded-lg h-full backdrop-blur-xs bg-white/55 md:bg-white/35 shadow-[0_4px_30px_rgba(0,0,0,0.1)] md:border md:border-white">
+        <CardContent className="w-full h-full flex flex-col gap-4">
+          <div className="w-full flex justify-center md:hidden select-none relative">
+            <Image
+              className="h-10 w-auto"
+              src="/images/placeholders/logoipsum/logoipsum-288.svg"
+              alt="Logo"
+              width={100}
+              height={100}
+            />
+          </div>
+          <div className="w-full mt-12">{children}</div>
+        </CardContent>
+      </Card>
     </div>
   );
 }
--- app/(guest)/auth/signin/page.tsx
+++ app/(guest)/auth/signin/page.tsx
@@ -1,7 +1,15 @@
+import { SignInForm } from "@/features/auth/components/forms";
+
 export const metadata = {
   title: "Masuk - eFin PT Kinarya Alam Raya",
+  description:
+    "Masuk ke dashboard e-Fin PT Kinarya Alam Raya untuk mengelola dan memantau informasi keuangan perusahaan secara efisien dan aman.",
 };
 
 export default function SignInPage() {
-  return <div>SignIn</div>;
+  return (
+    <div className="w-full flex flex-col items-center gap-4">
+      <SignInForm />
+    </div>
+  );
 }
