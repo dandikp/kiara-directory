0abbdde22b63f3ea0c62ec7891a2dd37fdf9313a
Dandi Pratama <mas.gondronglabanan@gmail.com>
2025-04-16 16:37:17 +0700

    feat: wip admin

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -9,6 +9,7 @@ const UserBaseLayout = ({
   return (
     <SidebarProvider>
       <AppSidebar items={[]} />
+      <main className="relative flex-1 space-y-6 h-full max-h-dvh overflow-y-auto"></main>
       {children}
     </SidebarProvider>
   );
