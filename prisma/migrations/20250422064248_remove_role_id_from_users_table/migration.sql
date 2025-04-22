2543aa47d38e6f4d8c7c07e6f1eba9c8cb6be589
Dandi Pratama <mas.gondronglabanan@gmail.com>
2025-04-22 14:43:20 +0700

    feat: remove roleId from users table

---
+++ prisma/migrations/20250422064248_remove_role_id_from_users_table/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `roleId` on the `users` table. All the data in the column will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE `users` DROP FOREIGN KEY `users_roleId_fkey`;
+
+-- DropIndex
+DROP INDEX `users_roleId_fkey` ON `users`;
+
+-- AlterTable
+ALTER TABLE `users` DROP COLUMN `roleId`;
